dff2968e1508a6e4977746fffcc1e15eb5d703a9
miw-upm <miw@eui.upm.es>
2014-11-11 17:31:21 +0100

    contexto

--- src/main/java/servlets/ContextoGet.java
+++ src/main/java/servlets/ContextoGet.java
@@ -12,19 +12,19 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Contexto
  */
-@WebServlet("/Contexto")
-public class Contexto extends HttpServlet {
+@WebServlet("/Contexto2")
+public class ContextoGet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        this.getServletContext().setAttribute("Web", "Se comparte entre servlets");
-        request.getSession().setAttribute("Sesion", "ya visto");
-        request.setAttribute("Peticion", "Al dar la respuesta al cliente se pierde");
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         out.println("<html><head><title>Parámetros</title> </head><body>");
-        out.println("<p>Contextos almacenados</p></body></html>");
+        out.println("<p> Contexto Web: " + this.getServletContext().getAttribute("Web") + "</p>");
+        out.println("<p> Contexto sesion: " + request.getSession().getAttribute("Sesion") + "</p>");
+        out.println("<p> Contexto peticion: " + request.getAttribute("Peticion") + "</p>");
+        out.println("</body></html>");
     }
 
 }
--- src/main/java/servlets/ContextoSet.java
+++ src/main/java/servlets/ContextoSet.java
@@ -12,19 +12,19 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Contexto
  */
-@WebServlet("/Contexto2")
-public class Contexto2 extends HttpServlet {
+@WebServlet("/ContextoSet")
+public class ContextoSet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        this.getServletContext().setAttribute("Web", "Se comparte entre servlets");
+        request.getSession().setAttribute("Sesion", "ya visto");
+        request.setAttribute("Peticion", "Al dar la respuesta al cliente se pierde");
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         out.println("<html><head><title>Parámetros</title> </head><body>");
-        out.println("<p> Contexto Web: " + this.getServletContext().getAttribute("Web") + "</p>");
-        out.println("<p> Contexto sesion: " + request.getSession().getAttribute("Sesion") + "</p>");
-        out.println("<p> Contexto peticion: " + request.getAttribute("Peticion") + "</p>");
-        out.println("</body></html>");
+        out.println("<p>Contextos almacenados</p></body></html>");
     }
 
 }
